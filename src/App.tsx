c790f735bae9e1872215b3d5cefe1a7a56f7baeb
sakots <sakots8000@gmail.com>
2026-03-11 08:50:26 +0900

    h1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css'
+import './style/App.css'
 
 function App() {
 
   return (
     <>
+      <h1>katakata-lite</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
